41c35c9099b694d3254040664081435b611f1635
Gaurav Anand <gouravanand08@gmail.com>
2020-12-05 23:34:44 +0530

    feat:html,styles and js updated

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Translate English Language into Minion's Language
+api calls used for Translation
--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var translateButton = document.querySelector("#translate");
-translateButton.addEventListener("click",function getInput(){
-    var input = document.querySelector("#inputTxt").value;
+var inputBox = document.querySelector("#inputTxt");
+var outputBox = document.querySelector("#outputTxt");
 
-});
-function getJsonResponseFromApi(input){
-    var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-    var argument = "?text="+"test";
-    var request = url+argument;
-    fetch(request);
+function errorHandler(error){
+    console.log("Error: "+error);
+    alert("OOPS! Something went wrong. For more information on error check console");
 }
+
+function getRequest(input){
+    var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+    var req = url+"?text="+input;
+    console.log(req);
+    return req;
+}
+
+translateButton.addEventListener("click",function getJsonResponseFromApi(){
+    var input = inputBox.value;
+    var request = getRequest(input);
+    fetch(request).then(response => response.json()).then(jsonData => outputBox.innerText = jsonData.contents.translated).catch(errorHandler);
+});
